c8c80ad4db1955dc85d5a89e7b125af7e146e313
Elemson Ifeanyi <ielemson>
2020-05-30 20:19:29 +0100

    edited 404 page

--- src/components/pages/notfound/notfound.js
+++ src/components/pages/notfound/notfound.js
@@ -1,27 +1,62 @@
-
-import React, {useEffect, Fragment } from 'react';
-import {Link} from "react-router-dom";
-import {Helmet} from "react-helmet"
-function Notfound() {
-  // Similar to componentDidMount and componentDidUpdate:
-  useEffect(() => {
- document.getElementById("page-body").classList.add("four-zero-four");
-  });
+import React from "react";
+import { Helmet } from "react-helmet";
+import "./notfound.css";
+// import { Link } from "react-router-dom";
+import { Header, SideBar, PageHeaderTitle, Footer } from "../../partials";
+// import Content from "../main";
+const Notfound = () => {
+  // componentDidMount() {
+  //   document.getElementById("home").classList.add("active");
+  // }
 
   return (
-    <Fragment>
-         <Helmet>
-<title>Error 404</title>
-</Helmet>
-    <div className="four-zero-four-container">
-    <div className="error-code">404</div>
-    <div className="error-message">This page doesn't exist</div>
-    <div className="button-place">
-        <Link to="/" className="btn btn-default btn-lg waves-effect">GO TO HOMEPAGE</Link>
-    </div>
-</div>
-</Fragment>
-  );
-}
+    <div className="page notfound">
+      <Helmet>
+        <title>404</title>
+      </Helmet>
+      {/* HEADER PART */}
+      <Header />
+      {/* CLOSE HEADER PART */}
 
-export default Notfound
+      {/* SIDER BAR PART */}
+      <div className="page-content d-flex align-items-stretch">
+        <SideBar />
+
+        <div className="content-inner">
+          {/* <!-- Page Header--> */}
+          <PageHeaderTitle title="Dashboard" currpg="404" />
+
+          {/* <!-- Dashboard Counts Section--> */}
+          <section className="dashboard-counts no-padding-bottom ">
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="error-template">
+                    <h1>Oops!</h1>
+                    <h2>404 Not Found</h2>
+                    <div className="error-details">
+                      Sorry, an error has occured, Requested page not found!
+                    </div>
+                    <div className="error-actions">
+                      <a href="/dashboard" className="btn btn-primary btn-lg">
+                        <span className="fa fa-home"></span> Take Me Home{" "}
+                      </a>
+                      <a href="#!" className="btn btn-default btn-lg">
+                        <span className="fa fa-envelope"></span> Contact Support{" "}
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <Footer />
+          {/* <!-- Dashboard Header Section    --> */}
+        </div>
+      </div>
+      {/* CLOSE SIDE BAR */}
+    </div>
+  );
+};
+
+export default Notfound;
