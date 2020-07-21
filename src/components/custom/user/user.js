e46401b84d58ca691727de595bcd2d82bcb03034
Elemson Ifeanyi <ielemson>
2020-07-21 23:27:37 +0100

    removed unused vars

--- src/components/custom/user/user.js
+++ src/components/custom/user/user.js
@@ -6,13 +6,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
-// import PhoneIcon from '@material-ui/icons/Phone';
-// import FavoriteIcon from '@material-ui/icons/Favorite';
-// import PersonPinIcon from '@material-ui/icons/PersonPin';
-// import HelpIcon from '@material-ui/icons/Help';
-// import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
-// import ThumbDown from '@material-ui/icons/ThumbDown';
-// import ThumbUp from '@material-ui/icons/ThumbUp';
 import Typography from '@material-ui/core/Typography';
 import { useHistory } from "react-router-dom";
 import { Bio } from "./index"
@@ -110,7 +103,6 @@ const User = (props) => {
   );
 
 }
-
 
 TabContainer.propTypes = {
   children: PropTypes.node.isRequired,
