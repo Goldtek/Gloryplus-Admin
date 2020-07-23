657d3e6e892329445b3ac3e39d9bd0abc18ae524
Elemson Ifeanyi <ielemson>
2020-07-23 23:35:46 +0100

    updated role

--- src/_helpers/role.js
+++ src/_helpers/role.js
@@ -1,4 +1,18 @@
 export const Role = {
+    SA: "SA",
     Admin: 'Admin',
-    User: 'User'    
+    User: 'User',
+    HFL: "HFL",
+    MVP: "MVP",
+    SEC: "SEC"
 }
+
+
+/*
+
+SA :super admin
+MVP: most important person
+SEC:secretary
+HFL:
+
+ */
